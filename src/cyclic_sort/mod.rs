8c4688bfe76a69d607b5023e3d12e345201a0dbd
Gabriel <gabriel@underdog.io>
2021-03-28 15:09:26 -0400

    Fixed cyclic sort

--- src/cyclic_sort/mod.rs
+++ src/cyclic_sort/mod.rs
@@ -1,15 +1,14 @@
-pub fn cyclic_sort(&nums: Vec<i32>) -> Vec<i32> {
+pub fn cyclic_sort(nums: &mut Vec<usize>) -> Vec<usize> {
     let mut i = 0;
-
     while i < nums.len() {
-        if nums[i] < nums.len() && nums[i] != nums[nums[i]] {
-            nums.swap(i, nums[i])
+        let j = nums[i] - 1;
+        if nums[i] != nums[j] {
+            nums.swap(i, j);
         } else {
-            i += 1
+            i += 1;
         }
     }
-
-    return nums
+    return nums.to_vec();
 }
 
 #[cfg(test)]
@@ -18,6 +17,6 @@ mod tests {
 
     #[test]
     fn sort_numbers() {
-        assert_eq!(cyclic_sort(vec![6,5,1,2,4,3]), [1,2,3,4,5,6])
+        assert_eq!(cyclic_sort(&mut vec![6, 5, 1, 2, 4, 3]), [1,2,3,4,5,6])
     }
 }
